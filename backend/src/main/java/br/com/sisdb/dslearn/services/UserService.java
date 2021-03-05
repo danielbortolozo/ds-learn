7ca695289528d630732fad953388fae863286f67
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2021-03-04 21:54:27 -0300

    Validation, exception handling

--- backend/src/main/java/br/com/sisdb/dslearn/services/UserService.java
+++ backend/src/main/java/br/com/sisdb/dslearn/services/UserService.java
@@ -1,22 +1,18 @@
 package br.com.sisdb.dslearn.services;
 
+import br.com.sisdb.dslearn.dto.UserDTO;
 import br.com.sisdb.dslearn.entities.User;
 import br.com.sisdb.dslearn.repositories.UserRepository;
+import br.com.sisdb.dslearn.services.exceptions.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
@@ -37,4 +33,13 @@ public class UserService implements UserDetailsService {
         logger.info("User found: "+username);
         return user;
     }
+
+    @Transactional(readOnly = true)
+    public UserDTO findById (Long id) {
+        Optional<User> obj = repository.findById(id);
+        User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found."));
+        return new UserDTO(entity);
+    }
+
+
 }
